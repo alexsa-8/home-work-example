ec99ad7b78ab7e01ffd7d37a9d24ee0787d14a75
Alex <alexsa-8@mail.ru>
2022-07-28 21:25:15 +0300

    выполнил ДЗ номер 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Пишем наше ДЗ!!");
+        System.out.println("Делаем коммит и пуш");
+        System.out.println("Переходим в гитхаб");
     }
 }
